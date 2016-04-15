5bcdb07de572f8e5d70cebc26c89a61e5b75ad40
zekor514 <zekor@me.com>
2016-04-15 11:10:23 +0100

    Simple change

--- TestGitHub/ViewController.swift
+++ TestGitHub/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("Cannot wait to put this project to GitHub")
     }
 
     override var representedObject: AnyObject? {
